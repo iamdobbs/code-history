a987f6c94313add1d63d2eace5c395f9ac4f8b61
Hayley Dobbs <iamdobbs@icloud.com>
2023-05-05 18:53:53 +0100

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Hayley Dobbs on 05/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
