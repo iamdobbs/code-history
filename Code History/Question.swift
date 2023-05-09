e92d80176f7713faebd31b057d5fd17e7ce62b10
Hayley Dobbs <iamdobbs@icloud.com>
2023-05-09 11:34:35 +0100

    Create Question struct

---
+++ Code History/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Hayley Dobbs on 09/05/2023.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
