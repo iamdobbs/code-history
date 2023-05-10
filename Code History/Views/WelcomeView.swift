29b12214c2eb69f098f389b3157e9dada04b7be6
Hayley Dobbs <iamdobbs@icloud.com>
2023-05-10 16:50:21 +0100

    Update to use GameColor and BottomTextView

--- Code History/Views/WelcomeView.swift
+++ Code History/Views/WelcomeView.swift
@@ -8,13 +8,11 @@
 import SwiftUI
 
 struct WelcomeView: View {
-    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     
     var body: some View {
         NavigationView {
             ZStack {
-                mainColor.ignoresSafeArea()
+                GameColor.main.ignoresSafeArea()
                 VStack {
                     Spacer()
                     VStack(alignment: .leading, spacing: 0) {
@@ -30,12 +28,12 @@ struct WelcomeView: View {
                         label: {
                             HStack {
                                 Spacer()
-                                Text("For sure, quiz me!")
+                                BottomTextView(str: "For sure, quiz me!")
                                     .font(.body)
                                     .bold()
                                     .padding()
                                 Spacer()
-                            }.background(accentColor)
+                            }.background(GameColor.accent)
                         })
                 }
                 .foregroundColor(.white)
