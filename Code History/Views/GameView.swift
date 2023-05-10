f0c91ac60f9b615491422498376ec59d97700a8b
Hayley Dobbs <iamdobbs@icloud.com>
2023-05-10 16:49:40 +0100

    Update to use GameViewModel and GameColorView

--- Code History/Views/GameView.swift
+++ Code History/Views/GameView.swift
@@ -14,13 +14,13 @@ struct GameView: View {
         possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
         correctAnswerIndex: 2)
     
-    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @StateObject var viewModel = GameViewModel()
     
     var body: some View {
         ZStack {
-            mainColor.ignoresSafeArea()
+            GameColor.main.ignoresSafeArea()
             VStack{
-                Text("1 /10")
+                Text(viewModel.questionProgressText)
                     .font(.callout)
                     .multilineTextAlignment(.leading)
                     .padding()
@@ -33,7 +33,6 @@ struct GameView: View {
                     ForEach(0..<question.possibleAnswers.count) { answerIndex in
                         Button(action: {
                             print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
-                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
                         }, label: {
                             ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
                         })
