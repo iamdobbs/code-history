3a2780a7cc172e3e107a1f94009c604536db3394
Hayley Dobbs <iamdobbs@icloud.com>
2023-05-10 17:04:45 +0100

    Add EnvironmentObject and functionality for recognising guess made

--- Code History/Views/QuestionView.swift
+++ Code History/Views/QuestionView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct QuestionView: View {
  
-  let question: Question
+    @EnvironmentObject var viewModel: GameViewModel
+    let question: Question
  
   var body: some View {
     VStack {
@@ -22,11 +23,17 @@ struct QuestionView: View {
         ForEach(0..<question.possibleAnswers.count) { answerIndex in
           Button(action: {
             print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+              viewModel.makeGuess(atIndex: answerIndex)
           }) {
             ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
           }
         }
       }
+        if viewModel.guessWasMade {
+            Button(action: { viewModel.displayNextScreen() }) {
+                BottomTextView(str: "Next")
+            }
+        }
     }
   }
 }
